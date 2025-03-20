5a5af5b85949d7df9e64471a7c5f0ad59d670ea9
Paweł Lewandowski <plewandowski555@gmail.com>
2025-03-20 14:14:49 +0100

    tests(#3): refactor tests

--- pkg/middleware/auth/test/session_manager_test.go
+++ pkg/middleware/auth/test/session_manager_test.go
@@ -7,18 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestSessionManager_AddAndGetSession tests the AddSession and GetSession methods of the SessionManager.
-// It verifies that sessions can be added and retrieved correctly.
-// It also verifies that sessions can be retrieved by both sessionNonce and peerIdentityKey.
-// It also verifies that the "best" session is retrieved when multiple sessions are associated with the same peerIdentityKey
-func TestSessionManager_AddAndGetSession(t *testing.T) {
+func TestSessionManager_HappyPath(t *testing.T) {
 	sessionManager := mock.NewSessionManager()
 
 	t.Run("Add and get session by both keys", func(t *testing.T) {
+		// given
 		session := mock.NewPeerSession(t)
 
+		// when
 		sessionManager.AddSession(session)
 
+		// then
 		retrievedSession := sessionManager.GetSession(*session.SessionNonce)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, session, *retrievedSession)
@@ -29,79 +28,121 @@ func TestSessionManager_AddAndGetSession(t *testing.T) {
 	})
 
 	t.Run("Correctly get best session by both keys", func(t *testing.T) {
+		// given
 		sessions := mock.NewPeerSessionsForThisSameIdentityKey(t, 5)
 		identityKey := *sessions[0].PeerIdentityKey
 
+		// when
 		sessionManager.AddSession(sessions[0])
 
-		// The "best" session should be the only one
+		// then - the "best" session should be the only one
 		retrievedSession := sessionManager.GetSession(identityKey)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, sessions[0], *retrievedSession)
 
+		// when
 		sessionManager.AddSession(sessions[1])
 
-		// The "best" session should be the most recent one
+		// then - the "best" session should be the most recent one
 		retrievedSession = sessionManager.GetSession(identityKey)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, sessions[1], *retrievedSession)
 
+		// when
 		sessions[2].IsAuthenticated = true
 		sessionManager.AddSession(sessions[2])
 
-		// The "best" session should be the authenticated one
+		// then - the "best" session should be the authenticated one
 		retrievedSession = sessionManager.GetSession(identityKey)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, sessions[2], *retrievedSession)
 
+		// when
 		sessionManager.AddSession(sessions[3])
 
-		// The "best" session should still be the authenticated one
+		// then - the "best" session should still be the authenticated one
 		retrievedSession = sessionManager.GetSession(identityKey)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, sessions[2], *retrievedSession)
 
+		// when
 		sessions[4].IsAuthenticated = true
 		sessionManager.AddSession(sessions[4])
 
-		// The "best" session should be the most recent authenticated one
+		// then - the "best" session should be the most recent authenticated one
 		retrievedSession = sessionManager.GetSession(identityKey)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, sessions[4], *retrievedSession)
 	})
-}
-
-func TestSessionManager_UpdateSession(t *testing.T) {
-	sessionManager := mock.NewSessionManager()
 
 	t.Run("Update session", func(t *testing.T) {
+		// given
 		session := mock.NewPeerSession(t)
-
 		sessionManager.AddSession(session)
 
+		// when
 		session.IsAuthenticated = true
 		sessionManager.UpdateSession(session)
 
+		// then
 		retrievedSession := sessionManager.GetSession(*session.SessionNonce)
 		require.NotNil(t, retrievedSession)
 		require.Equal(t, session, *retrievedSession)
 	})
-}
-
-func TestSessionManager_RemoveSession(t *testing.T) {
-	sessionManager := mock.NewSessionManager()
 
 	t.Run("Remove session", func(t *testing.T) {
+		// given
 		session := mock.NewPeerSession(t)
-
 		sessionManager.AddSession(session)
 
+		// when
 		sessionManager.RemoveSession(session)
 
+		// then
 		retrievedSession := sessionManager.GetSession(*session.SessionNonce)
 		require.Nil(t, retrievedSession)
 
 		retrievedSession = sessionManager.GetSession(*session.PeerIdentityKey)
 		require.Nil(t, retrievedSession)
+	})
+}
+
+func TestSessionManager_ErrorPath(t *testing.T) {
+	sessionManager := mock.NewSessionManager()
+
+	t.Run("Get non-existent session", func(t *testing.T) {
+		// given
+		invalidKey := "non-existent-key"
+
+		// when
+		retrievedSession := sessionManager.GetSession(invalidKey)
+
+		// then
+		require.Nil(t, retrievedSession)
+	})
+
+	t.Run("Remove non-existent session", func(t *testing.T) {
+		// given
+		session := mock.NewPeerSession(t)
+
+		// when
+		sessionManager.RemoveSession(session)
+
+		// then
+		retrievedSession := sessionManager.GetSession(*session.SessionNonce)
+		require.Nil(t, retrievedSession)
+	})
+
+	t.Run("Update non-existent session", func(t *testing.T) {
+		// given
+		session := mock.NewPeerSession(t)
+
+		// when
+		sessionManager.UpdateSession(session)
+
+		// then
+		retrievedSession := sessionManager.GetSession(*session.SessionNonce)
+		require.NotNil(t, retrievedSession)
+		require.Equal(t, session, *retrievedSession)
 	})
 }
